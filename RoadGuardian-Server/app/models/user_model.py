d7efc6d2d56e9c975df3fda367cd4be7f8c41b02
Lorenzo-Olivola <l.olivola@studenti.unisa.it>
2025-12-04 01:53:52 +0100

    feat: user_model.py
    Creazioni delle classi da output UserModelDTO, UserModelChangeDTO.
    Crezioni delle classi di input UserCreateInput, UserUpdateInput.
    Per poter comunicare sia con il cliente che con il database.

--- RoadGuardian-Server/app/models/user_model.py
+++ RoadGuardian-Server/app/models/user_model.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
 from typing import Optional
 from pydantic_extra_types.phone_numbers import PhoneNumber
 
@@ -25,4 +25,46 @@ class UserModel(BaseModel):
                 "num_tel": "+393332984046",
                 "role": "admin"
             }
-        } 
+        }
+
+class UserModelDTO(BaseModel):
+    """Classe per output utente al client senza password"""
+    id: Optional[str] = Field(default=None, alias="_id") 
+    email: EmailStr #forse da implementare in schema
+    first_name: str
+    last_name: str
+    num_tel: PhoneNumber #forse da implementare in schema
+    is_active: bool = True #account attivo o eliminato
+    role: str = "user" #user o admin
+
+    model_config = ConfigDict(populate_by_name = True)
+
+class UserModelChangeDTO(BaseModel):
+    """Classe per modifica e cancellazione utente"""
+    email: Optional[EmailStr] #forse da implementare in schema
+    first_name: Optional[str]
+    last_name: Optional[str]
+    password: Optional[str]
+    num_tel: Optional[PhoneNumber] #forse da implementare in schema
+    is_active: Optional[bool] = True #account attivo o eliminato
+    role: str = "user" #user o admin
+
+    model_config = ConfigDict(populate_by_name = True)
+
+
+class UserCreateInput(BaseModel):
+    """Modello per la crezione (Input dal client - tutti obbligatori)"""
+    first_name: str
+    last_name: str
+    email: EmailStr
+    password: str  # Password in chiaro inserita dall'utente
+    num_tel: PhoneNumber
+
+
+class UserUpdateInput(BaseModel):
+    """Modello per l'AGGIORNAMENTO (Input dal client - tutti opzionali)"""
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+    num_tel: Optional[PhoneNumber] = None
