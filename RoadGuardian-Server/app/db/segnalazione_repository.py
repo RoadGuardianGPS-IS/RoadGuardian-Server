217f0ae7f9871a5702e46ce8020f9261a248d4f7
Axel-o-four <157743174+Axel-o-four@users.noreply.github.com>
2025-12-07 00:32:40 +0100

    Fix: formato data
    Modificata la gestione delle date, affinché sia possibile memorizzarle, con combine() e to_mongo()

--- RoadGuardian-Server/app/db/segnalazione_repository.py
+++ RoadGuardian-Server/app/db/segnalazione_repository.py
@@ -1,5 +1,5 @@
 from db.connection import get_database
-# from models.incident_model import IncidentModel
+from models.incident_model import IncidentModel
 from bson import ObjectId
 from pymongo import ReturnDocument
 import datetime
@@ -8,14 +8,14 @@ import datetime
 db = get_database()
 segnalazione_collection = db["segnalazioni"]  # "segnalazioni" è il nome della collection che vedrai su Compass
 
-def create_segnalazione(segnalazione: dict) -> dict:
+def create_segnalazione(segnalazione: IncidentModel) -> dict:
     """Inserisce una segnalazione nel DB"""
-    segnalazione_dict = segnalazione.model_dump(by_alias=True, exclude={"id"}) # Escludiamo ID perché lo crea Mongo
+    segnalazione_dict = segnalazione.to_mongo() #chiama il metodo interno alla classe del model
     result = segnalazione_collection.insert_one(segnalazione_dict)
     
     # Recuperiamo l'ID generato e lo assegniamo all'oggetto
-    segnalazione["id"] = str(result.inserted_id)
-    return segnalazione
+    segnalazione_dict["id"] = str(result.inserted_id)
+    return segnalazione_dict
 
 def get_segnalazione_by_id(segnalazione_id: str) -> dict | None:
     """Cerca una segnalazione per ID"""
@@ -64,31 +64,42 @@ def get_segnalazione_by_status(status: bool) -> list[dict]:
 
     return list(segnalazione_collection.find({"status": status}))
 
-def get_segnalazione_by_date(date: datetime.date) -> list[dict]:
-    """Cerca segnalazioni per data
+def get_segnalazione_by_date(target_date: datetime.date) -> list[dict]:
+    """Cerca segnalazioni per data, dalle 00:00 alle 23:59 (min e max)
     ATTENZIONE: questa funzione ritorna una lista di segnalazioni"""
+    start_dt = datetime.datetime.combine(target_date, datetime.time.min)
+    end_dt = datetime.datetime.combine(target_date, datetime.time.max)
 
     return list(segnalazione_collection.find({
-        "date": date,
+        "incident_date": {
+            "$gte": start_dt, # Maggiore o uguale a inizio giorno
+            "$lte": end_dt    # Minore o uguale a fine giorno
+        },
         "status": True
     }))
+    
 
-def get_segnalazione_by_time(time: datetime.time) -> list[dict]:
+def get_segnalazione_by_time(target_time: datetime.time) -> list[dict]:
     """Cerca segnalazioni per orario
     ATTENZIONE: questa funzione ritorna una lista di segnalazioni"""
-
     return list(segnalazione_collection.find({
-        "time": time,
+        "$expr": {
+            "$and": [
+                # Confronta l'ora del campo DB con l'ora richiesta
+                {"$eq": [{"$hour": "$incident_date"}, target_time.hour]},
+                # Confronta i minuti del campo DB con i minuti richiesti
+                {"$eq": [{"$minute": "$incident_date"}, target_time.minute]},
+            ]
+        },
         "status": True
     }))
 
-def get_segnalazione_by_date_and_time(date_and_time: datetime.datetime) -> list[dict]:
+def get_segnalazione_by_date_and_time(target_date: datetime.date, target_time: datetime.time) -> list[dict]:
     """Cerca segnalazioni per data e orario
     ATTENZIONE: questa funzione ritorna una lista di segnalazioni"""
-
+    dt_to_find = datetime.datetime.combine(target_date, target_time)
     return list(segnalazione_collection.find({
-        "date": date_and_time.date(),
-        "time": date_and_time.time(),
+        "incident_date": dt_to_find,
         "status": True
     }))
 
@@ -108,7 +119,7 @@ def delete_segnalazione(segnalazione_id: str) -> bool:
         oid = ObjectId(segnalazione_id)
         result = segnalazione_collection.update_one({
             "_id": oid},
-            {"$set": {"status": False}})
+            {"$set": {"status": False}}) #Per "eliminare" la segnalazione cambia lo status di essa in false, come avviene con la cancellazione del profilo utente
         return result.modified_count > 0
     except Exception as e:
         print(f"Errore delete_segnalazione: {e}")
